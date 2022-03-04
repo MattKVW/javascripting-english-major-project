501764199e315ee4614c97baa0b5fd27752e283b
MattKVW <byrnematthew12@gmail.com>
2022-03-04 10:16:52 -0500

    html

--- scripts.js
+++ scripts.js
@@ -17,21 +17,21 @@ $("#response").html(arrayOfStrings[2]);
 */
 
 let myHabaneroSauceSquirts, myBurritoObject;
-myHabeneroSauceSquirts 3;
+myHabaneroSauceSquirts = 3;
 myBurritoObject = {
-    tortila: "wheat",
-    guacamole: true,
-    beans: "pinto",
-    habaneroSauceSquirts: myHabaneroSauceSquirts,
-    spiciness: function(){
-      if (myHabaneroSauceSquirts > 0 ){
-        alert("This is a spicy burrito!");
-      } else {
-        alert("This is a mild burrito.");
-      }
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0 ){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
     }
+  }
 };
-$("#response".html("Your burrito has " +
- myBurritoObject.habaneroSauceSquirts +
- " squirts of habanero.");
- myBurritoObject.spiciness();
+$("#response").html("Your burrito has " +
+  myBurritoObject.habaneroSauceSquirts +
+  " squirts of habanero.");
+myBurritoObject.spiciness();
