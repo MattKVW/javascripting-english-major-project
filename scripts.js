be7022008fcd054613aff19c21872eca6a9a4c10
MattKVW <byrnematthew12@gmail.com>
2022-03-04 09:37:05 -0500

    spicy

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*
 let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
@@ -5,3 +6,32 @@ if ( userInput === "burrito" ) {
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
+
+/*
+let arrayOfStrings, arrayOfNumbers, arrayMixed;
+arrayOfStrings = ["a", "b", "c"];
+arrayOfNumbers = [1, 2, 3];
+arrayMixed = ["a", null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayOfStrings[2]);
+*/
+
+let myHabaneroSauceSquirts, myBurritoObject;
+myHabeneroSauceSquirts 3;
+myBurritoObject = {
+    tortila: "wheat",
+    guacamole: true,
+    beans: "pinto",
+    habaneroSauceSquirts: myHabaneroSauceSquirts,
+    spiciness: function(){
+      if (myHabaneroSauceSquirts > 0 ){
+        alert("This is a spicy burrito!");
+      } else {
+        alert("This is a mild burrito.");
+      }
+    }
+};
+$("#response".html("Your burrito has " +
+ myBurritoObject.habaneroSauceSquirts +
+ " squirts of habanero.");
+ myBurritoObject.spiciness();
